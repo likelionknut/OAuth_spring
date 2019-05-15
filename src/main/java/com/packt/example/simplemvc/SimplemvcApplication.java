ad1b4e5c4bc36b6dfae34af693a2d317f8e58b98
knut-sdpark <sdp1422@naver.com>
2019-05-15 14:17:39 +0900

    create 'spring 초기 파일 모두'

---
+++ src/main/java/com/packt/example/simplemvc/SimplemvcApplication.java
@@ -0,0 +1,14 @@
+package com.packt.example.simplemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+public class SimplemvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplemvcApplication.class, args);
+	}
+
+}

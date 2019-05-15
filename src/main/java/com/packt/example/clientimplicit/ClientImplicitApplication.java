f676a56c93aa57010e96e7978fb90268eff4e302
knut-sdpark <sdp1422@naver.com>
2019-05-15 16:28:13 +0900

    created 'facebook test를 위해 프로젝트 생성' - '프로젝트 명 : client-implicit'

---
+++ src/main/java/com/packt/example/clientimplicit/ClientImplicitApplication.java
@@ -0,0 +1,13 @@
+package com.packt.example.clientimplicit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientImplicitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientImplicitApplication.class, args);
+	}
+
+}

c0b611af31089cd4a14d79bedb4c148811157dc8
knut-sdpark <sdp1422@naver.com>
2019-05-15 17:48:32 +0900

    add 'client()' 함수 'ClientImplicitApplication.java'

--- src/main/java/com/packt/example/clientimplicit/ClientImplicitApplication.java
+++ src/main/java/com/packt/example/clientimplicit/ClientImplicitApplication.java
@@ -3,6 +3,7 @@ package com.packt.example.clientimplicit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller @SpringBootApplication
 public class ClientImplicitApplication {
@@ -10,5 +11,8 @@ public class ClientImplicitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClientImplicitApplication.class, args);
 	}
+
+	@GetMapping("/")
+	public String client() { return "client"; }
 
 }

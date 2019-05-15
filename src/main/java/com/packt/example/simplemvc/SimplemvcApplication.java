6650b69d98aa11942f526ce9a1af86264d2b143e
knut-sdpark <sdp1422@naver.com>
2019-05-15 14:56:29 +0900

    add '@Controller' 'SimplemvcApplication.java'

--- src/main/java/com/packt/example/simplemvc/SimplemvcApplication.java
+++ src/main/java/com/packt/example/simplemvc/SimplemvcApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 
-@SpringBootApplication
+@Controller @SpringBootApplication
 public class SimplemvcApplication {
 
 	public static void main(String[] args) {

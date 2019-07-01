5f707b38b4f1570359616fe0ff2dda413e4fe995
knut-sdpark <sdp1422@naver.com>
2019-07-01 17:12:29 +0900

    added annotation - 'FacebookConfiguration.java'

--- src/main/java/com/packt/example/socialauthcode/facebook/FacebookConfiguration.java
+++ src/main/java/com/packt/example/socialauthcode/facebook/FacebookConfiguration.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.social.SocialAutoConfigurerAdapter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionFactory;
 import org.springframework.social.connect.ConnectionFactoryLocator;
@@ -13,6 +15,9 @@ import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.connect.web.ConnectController;
 import org.springframework.social.facebook.api.Facebook;
 
+
+@Configuration
+@EnableSocial
 public class FacebookConfiguration extends SocialAutoConfigurerAdapter {
 	@Autowired
 	private EnhancedFacebookProperties properties;

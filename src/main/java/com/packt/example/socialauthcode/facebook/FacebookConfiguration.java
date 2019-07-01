99c7b55b18ebee617a439f08f11867ef5a54d60f
knut-sdpark <sdp1422@naver.com>
2019-07-01 16:24:58 +0900

    added 'FacebookConfiguration.java' - 'connectController'

--- src/main/java/com/packt/example/socialauthcode/facebook/FacebookConfiguration.java
+++ src/main/java/com/packt/example/socialauthcode/facebook/FacebookConfiguration.java
@@ -8,25 +8,35 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionFactory;
+import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.connect.web.ConnectController;
 import org.springframework.social.facebook.api.Facebook;
 
 public class FacebookConfiguration extends SocialAutoConfigurerAdapter {
 	@Autowired
 	private EnhancedFacebookProperties properties;
-	
+
 	@Bean
 	@ConditionalOnMissingBean(Facebook.class)
 	@Scope(value = "request", proxyMode = ScopedProxyMode.INTERFACES)
 	public Facebook facebook(ConnectionRepository repository) {
-		Connection<Facebook> connection = repository
-				.findPrimaryConnection(Facebook.class);
+		Connection<Facebook> connection = repository.findPrimaryConnection(Facebook.class);
 		return connection != null ? connection.getApi() : null;
 	}
-	
+
 	@Override
 	protected ConnectionFactory<?> createConnectionFactory() {
-		return new CustomFacebookConnectionFactory(this.properties.getAppId(),
-				this.properties.getAppSecret(), this.properties.getApiVersion());
+		return new CustomFacebookConnectionFactory(this.properties.getAppId(), this.properties.getAppSecret(),
+				this.properties.getApiVersion());
+	}
+
+	@Bean
+	public ConnectController connectController(ConnectionFactoryLocator factoryLocator,
+			ConnectionRepository repository) {
+
+		ConnectController controller = new ConnectController(factoryLocator, repository);
+		controller.setApplicationUrl("http://localhost:8080");
+		return controller;
 	}
 }

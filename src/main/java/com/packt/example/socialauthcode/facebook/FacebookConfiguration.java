25979c95ae7bb1073bffbfbd16fac0bae34351b1
knut-sdpark <sdp1422@naver.com>
2019-07-01 16:06:48 +0900

    added 'FacebookConfiguration.java' - 'Facebook Bean'

--- src/main/java/com/packt/example/socialauthcode/facebook/FacebookConfiguration.java
+++ src/main/java/com/packt/example/socialauthcode/facebook/FacebookConfiguration.java
@@ -1,12 +1,28 @@
 package com.packt.example.socialauthcode.facebook;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.social.SocialAutoConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionFactory;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
 
 public class FacebookConfiguration extends SocialAutoConfigurerAdapter {
 	@Autowired
 	private EnhancedFacebookProperties properties;
+	
+	@Bean
+	@ConditionalOnMissingBean(Facebook.class)
+	@Scope(value = "request", proxyMode = ScopedProxyMode.INTERFACES)
+	public Facebook facebook(ConnectionRepository repository) {
+		Connection<Facebook> connection = repository
+				.findPrimaryConnection(Facebook.class);
+		return connection != null ? connection.getApi() : null;
+	}
 	
 	@Override
 	protected ConnectionFactory<?> createConnectionFactory() {

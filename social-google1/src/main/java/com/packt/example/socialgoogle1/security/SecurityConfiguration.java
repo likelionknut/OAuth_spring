d061b6f7b9fd915e84a88042f34c4d775066b373
knut-sdpark <sdp1422@naver.com>
2019-07-03 16:28:38 +0900

    added 'SecurityConfiguration.java' - 'configure()'

--- social-google1/src/main/java/com/packt/example/socialgoogle1/security/SecurityConfiguration.java
+++ social-google1/src/main/java/com/packt/example/socialgoogle1/security/SecurityConfiguration.java
@@ -1,9 +1,15 @@
 package com.packt.example.socialgoogle1.security;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/connect/google?*").permitAll().anyRequest().authenticated().and()
+				.formLogin().and().logout().permitAll().and().csrf().disable();
+	}
 }

e23bc41a32593f1c3ad03060ff767af93e813d30
knut-sdpark <sdp1422@naver.com>
2019-07-03 16:05:41 +0900

    added 'GoogleConfigurerAdapter.java' - 'addConnectionFactories()'

--- social-google1/src/main/java/com/packt/example/socialgoogle1/config/GoogleConfigurerAdapter.java
+++ social-google1/src/main/java/com/packt/example/socialgoogle1/config/GoogleConfigurerAdapter.java
@@ -3,12 +3,21 @@ package com.packt.example.socialgoogle1.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
 import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.config.annotation.SocialConfigurerAdapter;
+import org.springframework.social.google.connect.GoogleConnectionFactory;
 
 @Configuration @EnableSocial
 @EnableConfigurationProperties(GoogleProperties.class)
 public class GoogleConfigurerAdapter extends SocialConfigurerAdapter {
 	@Autowired
 	private GoogleProperties properties;
+	
+	@Override
+	public void addConnectionFactories(ConnectionFactoryConfigurer configurer, Environment environment) {
+		GoogleConnectionFactory factory = new GoogleConnectionFactory(this.properties.getAppId(), this.properties.getAppSecret());
+		configurer.addConnectionFactory(factory);
+	}
 }

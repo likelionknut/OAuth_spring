d2e3d8d2b24027cf60561f7b6e1f0bf4d5965e2f
knut-sdpark <sdp1422@naver.com>
2019-07-03 16:22:20 +0900

    added 'GoogleConfigurerAdapter.java' - 'getUsersConnectionRepository() & getUserIdSource()'

--- social-google1/src/main/java/com/packt/example/socialgoogle1/config/GoogleConfigurerAdapter.java
+++ social-google1/src/main/java/com/packt/example/socialgoogle1/config/GoogleConfigurerAdapter.java
@@ -7,13 +7,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.core.env.Environment;
+import org.springframework.social.UserIdSource;
 import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
 import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.config.annotation.SocialConfigurerAdapter;
 import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.mem.InMemoryUsersConnectionRepository;
 import org.springframework.social.google.api.Google;
 import org.springframework.social.google.connect.GoogleConnectionFactory;
+import org.springframework.social.security.AuthenticationNameUserIdSource;
 
 @Configuration @EnableSocial
 @EnableConfigurationProperties(GoogleProperties.class)
@@ -26,6 +31,16 @@ public class GoogleConfigurerAdapter extends SocialConfigurerAdapter {
 	public Google google(final ConnectionRepository repository) {
 		final Connection<Google> connection = repository.findPrimaryConnection(Google.class);
 		return connection != null ? connection.getApi() : null;
+	}
+	
+	@Override
+	public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
+		return new InMemoryUsersConnectionRepository(connectionFactoryLocator);
+	}
+	
+	@Override
+	public UserIdSource getUserIdSource() {
+		return new AuthenticationNameUserIdSource();
 	}
 	
 	@Override

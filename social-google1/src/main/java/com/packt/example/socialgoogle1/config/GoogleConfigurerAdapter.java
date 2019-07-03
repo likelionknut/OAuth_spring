ab7ef5db9b50c9e1f62b714cc78193e27b6ff885
knut-sdpark <sdp1422@naver.com>
2019-07-03 16:17:59 +0900

    added 'GoogleConfigurerAdapter.java' - '구글 API를 위한 DSL 제공하기 위한 메소드 추가'

--- social-google1/src/main/java/com/packt/example/socialgoogle1/config/GoogleConfigurerAdapter.java
+++ social-google1/src/main/java/com/packt/example/socialgoogle1/config/GoogleConfigurerAdapter.java
@@ -2,11 +2,17 @@ package com.packt.example.socialgoogle1.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.core.env.Environment;
 import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
 import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.config.annotation.SocialConfigurerAdapter;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.google.api.Google;
 import org.springframework.social.google.connect.GoogleConnectionFactory;
 
 @Configuration @EnableSocial
@@ -14,6 +20,13 @@ import org.springframework.social.google.connect.GoogleConnectionFactory;
 public class GoogleConfigurerAdapter extends SocialConfigurerAdapter {
 	@Autowired
 	private GoogleProperties properties;
+	
+	@Bean
+	@Scope(value = "request", proxyMode = ScopedProxyMode.INTERFACES)
+	public Google google(final ConnectionRepository repository) {
+		final Connection<Google> connection = repository.findPrimaryConnection(Google.class);
+		return connection != null ? connection.getApi() : null;
+	}
 	
 	@Override
 	public void addConnectionFactories(ConnectionFactoryConfigurer configurer, Environment environment) {

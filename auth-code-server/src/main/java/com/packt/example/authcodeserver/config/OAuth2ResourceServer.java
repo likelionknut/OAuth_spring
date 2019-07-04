d28c69e7f540237a9d892a27e38181ec6f504c7e
knut-sdpark <sdp1422@naver.com>
2019-07-04 17:29:24 +0900

    added 'OAuth2ResourceServer.java' - 'override configure()'

--- auth-code-server/src/main/java/com/packt/example/authcodeserver/config/OAuth2ResourceServer.java
+++ auth-code-server/src/main/java/com/packt/example/authcodeserver/config/OAuth2ResourceServer.java
@@ -1,6 +1,7 @@
 package com.packt.example.authcodeserver.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -8,4 +9,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class OAuth2ResourceServer extends ResourceServerConfigurerAdapter {
 
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().requestMatchers().antMatchers("/api/**");
+	}
 }

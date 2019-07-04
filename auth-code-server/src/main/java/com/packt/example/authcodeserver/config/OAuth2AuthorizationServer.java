f24bd851d739a87830cf7cc8337f63d97e31d258
knut-sdpark <sdp1422@naver.com>
2019-07-04 17:18:22 +0900

    created 'OAuth2AuthorizationServer.java' - 'extends AuthorizationServerConfigurerAdapter'

--- auth-code-server/src/main/java/com/packt/example/authcodeserver/config/OAuth2AuthorizationServer.java
+++ auth-code-server/src/main/java/com/packt/example/authcodeserver/config/OAuth2AuthorizationServer.java
@@ -1,5 +1,11 @@
 package com.packt.example.authcodeserver.config;
 
-public class OAuth2AuthorizationServer {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdapter {
 
 }

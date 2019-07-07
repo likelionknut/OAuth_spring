110d2eaf2ceb737db971fcf1a3caec99b7c05dd1
knut-sdpark <sdp1422@naver.com>
2019-07-07 16:20:49 +0900

    created 'password-server' project

---
+++ password-server/src/main/java/com/packt/example/passwordserver/PasswordServerApplication.java
@@ -0,0 +1,13 @@
+package com.packt.example.passwordserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordServerApplication.class, args);
+	}
+
+}

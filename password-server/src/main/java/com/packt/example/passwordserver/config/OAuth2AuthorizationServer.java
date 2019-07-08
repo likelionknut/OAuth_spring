f3e52ec10f348900531e47b2f21e20eb58a00897
knut-sdpark <sdp1422@naver.com>
2019-07-08 17:28:51 +0900

    added 'OAuth2AuthorizationServer.java'- 'AuthenticationManager property'

--- password-server/src/main/java/com/packt/example/passwordserver/config/OAuth2AuthorizationServer.java
+++ password-server/src/main/java/com/packt/example/passwordserver/config/OAuth2AuthorizationServer.java
@@ -1,6 +1,8 @@
 package com.packt.example.passwordserver.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -9,6 +11,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableAuthorizationServer
 public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdapter {
 
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory().withClient("clientapp").secret("123456").redirectUris("http://localhost:9000/callback")

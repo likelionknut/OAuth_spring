329fb03f1a2e12364de307703ea2cb197de493f8
knut-sdpark <sdp1422@naver.com>
2019-07-08 17:32:19 +0900

    added 'OAuth2AuthorizationServer.java' - 'Override configure()'

--- password-server/src/main/java/com/packt/example/passwordserver/config/OAuth2AuthorizationServer.java
+++ password-server/src/main/java/com/packt/example/passwordserver/config/OAuth2AuthorizationServer.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -13,7 +14,12 @@ public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdap
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager);
+	}
+
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory().withClient("clientapp").secret("123456").redirectUris("http://localhost:9000/callback")

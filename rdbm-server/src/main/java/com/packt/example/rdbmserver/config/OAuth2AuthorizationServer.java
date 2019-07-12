b8aa0ef5892dd5e02fdd8860d4810246efc42e97
knut-sdpark <sdp1422@naver.com>
2019-07-12 16:18:52 +0900

    added 'OAuth2AuthorizationServer.java' - 'sql 패키지의 DataSource 속성 추가'

--- rdbm-server/src/main/java/com/packt/example/rdbmserver/config/OAuth2AuthorizationServer.java
+++ rdbm-server/src/main/java/com/packt/example/rdbmserver/config/OAuth2AuthorizationServer.java
@@ -1,8 +1,26 @@
 package com.packt.example.rdbmserver.config;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import javax.sql.DataSource;
 
-public class OAuth2AuthorizationServer {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.jdbc(dataSource);
+	}
+
 	public static void main(String[] args) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(4);
 		String clientId = "clientapp";

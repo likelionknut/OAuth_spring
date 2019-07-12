c68391864892eedce779f36fc43b59575f78ad0d
knut-sdpark <sdp1422@naver.com>
2019-07-12 16:23:47 +0900

    added 'OAuth2AuthorizationServer.java' - 'TokenStore, ApprovalStore, PasswordEncoder'

--- rdbm-server/src/main/java/com/packt/example/rdbmserver/config/OAuth2AuthorizationServer.java
+++ rdbm-server/src/main/java/com/packt/example/rdbmserver/config/OAuth2AuthorizationServer.java
@@ -3,11 +3,17 @@ package com.packt.example.rdbmserver.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -15,6 +21,21 @@ public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdap
 
 	@Autowired
 	private DataSource dataSource;
+
+	@Bean
+	public TokenStore tokenStore() {
+		return new JdbcTokenStore(dataSource);
+	}
+
+	@Bean
+	public ApprovalStore approvalStore() {
+		return new JdbcApprovalStore(dataSource);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(4);
+	}
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {

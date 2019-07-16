9b60a9d875c0d532ce8aea3669de208dffb36c23
knut-sdpark <sdp1422@naver.com>
2019-07-16 18:22:12 +0900

    added 'ClientController.java' - 'register()'

--- oauth2provider/src/main/java/com/packt/example/oauth2provider/client/ClientController.java
+++ oauth2provider/src/main/java/com/packt/example/oauth2provider/client/ClientController.java
@@ -3,7 +3,9 @@ package com.packt.example.oauth2provider.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.provider.ClientRegistrationService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/client")
@@ -12,4 +14,11 @@ public class ClientController {
 	@Autowired
 	private ClientRegistrationService clientRegistrationService;
 
+	@GetMapping("/register")
+	public ModelAndView register(ModelAndView mv) {
+		mv.setViewName("client/register");
+		mv.addObject("registry", new BasicClientInfo());
+
+		return mv;
+	}
 }

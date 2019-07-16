39aa9acdd3bc4e6a0132e18bbb903f59e23bf5f2
knut-sdpark <sdp1422@naver.com>
2019-07-16 17:51:13 +0900

    added 'client/Application.java' - 'ClientDetails 인터페이스 & 속성'

--- oauth2provider/src/main/java/com/packt/example/oauth2provider/client/Application.java
+++ oauth2provider/src/main/java/com/packt/example/oauth2provider/client/Application.java
@@ -1,7 +1,102 @@
 package com.packt.example.oauth2provider.client;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.provider.ClientDetails;
 
 public class Application implements ClientDetails {
+	private static final long serialVersionUID = 1L;
+	
+	private String clientId;
+    private String clientSecret;
+    private ClientType clientType;
+    private Set<String> resourceIds = new HashSet<>();
+    private Set<String> scope = new HashSet<>();
+    private Set<String> webServerRedirectUri = new HashSet<>();
+    private int accessTokenValidity;
+    private Map<String, Object> additionalInformation = new HashMap<>();
+	
+	@Override
+	public String getClientId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<String> getResourceIds() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isSecretRequired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getClientSecret() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isScoped() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Set<String> getScope() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<String> getAuthorizedGrantTypes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<String> getRegisteredRedirectUri() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getAccessTokenValiditySeconds() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getRefreshTokenValiditySeconds() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAutoApprove(String scope) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Map<String, Object> getAdditionalInformation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
